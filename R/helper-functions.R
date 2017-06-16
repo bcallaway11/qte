ea792fe04165ea2649c72d08deb35300c6979d15
Brantly Callaway <brantly.callaway@temple.edu>
2017-06-15 21:27:48 -0400

    updated ggqte

--- R/helper-functions.R
+++ R/helper-functions.R
@@ -37,7 +37,7 @@ qtes2mat <- function(qteList, sset=NULL, se=TRUE, rnd=3) {
 #'
 #' @return a ggplot object
 #' @export
-ggqte <- function(qteobj, ylab="", xbreaks=c(.1,.3,.5,.7,.9)) {
+ggqte <- function(qteobj, ylab="", ylim=NULL, ybreaks=NULL, xbreaks=c(.1,.3,.5,.7,.9)) {
     tau <- qteobj$probs
     qte <- qteobj$qte
     qte.se <- qteobj$qte.se
@@ -50,7 +50,7 @@ ggqte <- function(qteobj, ylab="", xbreaks=c(.1,.3,.5,.7,.9)) {
                                         #geom_errorbar(size=.3, width=.02) + 
         ggplot2::geom_hline(yintercept=0) + 
         ggplot2::geom_point(aes(tau, qte)) +
-        ggplot2::scale_y_continuous(ylab) + ##, limits) +
+        ggplot2::scale_y_continuous(ylab, limits=ylim, breaks=ybreaks) + ##, limits) +
         ggplot2::scale_x_continuous("tau", limits=c(0,1), breaks=xbreaks) + 
         ggplot2::theme_classic() +
         ggplot2::theme(panel.border = element_rect(colour = 'black', size=1,
